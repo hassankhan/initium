9fc866226cc2690f6fd37caefa3b690f13f10876
Hassan Khan <contact@hassankhan.me>
2017-07-21 02:39:01 +0100

    Initial commit

---
+++ src/components/GitHub/FeedItem.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class FeedItem extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default FeedItem;
